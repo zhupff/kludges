4598a21544ecd624cbdfb41aa0c2aa634932eb8c
Zhupf <zhupff@qq.com>
2021-07-10 19:17:44 +0800

    dev先移除kludge-auto模块

--- kludge-auto-lib/src/main/kotlin/kludge/auto/Auto.kt
+++
@@ -1,9 +0,0 @@
-package kludge.auto
-
-/**
- * Author: Zhupf
- * Description: todo
- */
-@Retention(AnnotationRetention.SOURCE)
-@Target(AnnotationTarget.CLASS, AnnotationTarget.FIELD)
-annotation class Auto(val value: String = "")
